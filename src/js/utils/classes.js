31f983bfb8c0e2295f16301814d457627fb0f86f
Sefiane Chouaib <sefian.chouaib@gmail.com>
2025-09-01 23:30:37 +0100

    delete previous files

--- src/js/utils/classes.js
+++
@@ -1,7 +0,0 @@
-export const TARTIB_CLASSNAME = 'tartib';
-export const ITEM_CLASSNAME = 'tartib__item';
-export const ITEM_SELECTOR = '.' + ITEM_CLASSNAME;
-export const ITEM_DRAGGED_CLASSNAME = ITEM_CLASSNAME + '--dragged';
-export const PLACEHOLDER_CLASSNAME = 'tartib__placeholder';
-export const ELEVATION_CLASSNAME = 'tt-elevation';
-export const ACTIVE_CLASSNAME = 'tt-active';
